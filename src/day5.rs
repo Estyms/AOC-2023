749d55ae4cc76497e95344867282a8be00dcdd02
Evann <evann.regnault@pm.me>
2023-12-07 00:39:53 +0100

    Day 5 - Used fold

--- src/day5.rs
+++ src/day5.rs
@@ -69,11 +69,7 @@ fn process_maps(input: &str) -> IResult<&str, Vec<Map>> {
 
 
 fn seed_locations(seed: u64, maps: &Vec<Map>) -> u64 {
-    let mut number = seed;
-    for m in maps {
-        number = m.find_dest_number(number);
-    };
-    number
+    maps.iter().fold(seed, |num, m| m.find_dest_number(num))
 }
 
 
